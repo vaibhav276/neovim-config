7ca06e8ba7542af36d1d6c13b1330293b4525076
Vaibhav Pujari <vpujari@planful.com>
2026-06-11 06:07:32 +0000

    Bind neovim clipboard to system clipboard

--- init.lua
+++ init.lua
@@ -35,6 +35,7 @@ vim.opt.tabstop = 3      -- Number of spaces that a <Tab> in the file counts for
 vim.opt.shiftwidth = 3   -- Number of spaces to use for each step of (auto)indent
 vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.softtabstop = 3  -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt.clipboard = "unnamedplus"
 
 -- Comment below line until CopliotC-Mvim/CopilotChat.nvim is installed
 -- require("CopilotChat").setup()
