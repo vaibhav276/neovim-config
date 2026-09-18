2db373e5c972ad9ac91f4a2c8c081f597f537715
Vaibhav Pujari <vpujari@planful.com>
2026-09-18 06:40:06 +0000

    Argonaut theme

--- init.lua
+++ init.lua
@@ -22,6 +22,7 @@ Plug('airblade/vim-gitgutter')
 Plug('tpope/vim-dadbod')
 Plug('kristijanhusak/vim-dadbod-ui')
 Plug('EdenEast/nightfox.nvim')
+Plug('effkay/argonaut.vim')
 
 vim.call('plug#end')
 
